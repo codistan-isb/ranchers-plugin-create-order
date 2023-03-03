5bbb13191b43a39a5f5e2ca6e42e31a3f23d0730
shoaibfarooq3 <shoaib.farooq@codistan.org>
2023-03-03 12:03:59 +0500

    NewChanges

--- src/index.js
+++ src/index.js
@@ -1,73 +1,27 @@
 import { createRequire } from "module";
 import importAsString from "@reactioncommerce/api-utils/importAsString.js";
+import myResolvers from "./resolvers/resolvers.js"
 const mySchema = importAsString("./schema/schema.graphql");
-// const pkg = createRequire("../package.json");
 const require = createRequire(import.meta.url);
-// sadasdasd
-function myStartup(context) {
-  context.simpleSchemas.ProductVariant.extend({
-    Chassis: {
-      type: String,
-      min: 0,
-      optional: true,
-    },
-    Colour: {
-      type: String,
-      min: 0,
-      optional: true,
-    },
-    Sunroof: {
-      type: String,
-      min: 0,
-      optional: true,
-    },
-  });
+const pkg = require("../package.json");
 
-  context.simpleSchemas.CatalogProductVariant.extend({
-    Sunroof: {
-      type: String,
-      min: 0,
-      optional: true,
-    },
-    Colour: {
-      type: String,
-      min: 0,
-      optional: true,
-    },
-    Chassis: {
-      type: String,
-      min: 0,
-      optional: true,
-    },
-  });
-}
 
-function myPublishProductToCatalog(
-  catalogProduct,
-  { context, product, shop, variants }
-) {
-  catalogProduct.variants &&
-    catalogProduct.variants.map((catalogVariant) => {
-      const productVariant = variants.find(
-        (variant) => variant._id === catalogVariant.variantId
-      );
-      catalogVariant.Sunroof = productVariant.Sunroof || null;
-      catalogVariant.Colour = productVariant.Colour || null;
-      catalogVariant.Chassis = productVariant.Chassis || null;
-    });
-}
 async function register(app) {
   await app.registerPlugin({
-    label: "1",
-    name: "ProductNewFieldUpdation",
-    version: "0.1",
-    functionsByType: {
-      startup: [myStartup],
-      publishProductToCatalog: [myPublishProductToCatalog],
+    label: pkg.label,
+    name: pkg.name,
+    version: pkg.version,
+    collections: {
+      Service: {
+        name: "CreateOrder",
+        updatedAt: { type: Date, default: Date.now },
+        createdAt: { type: Date, default: Date.now },
+      },
     },
     graphQL: {
       schemas: [mySchema],
-    },
+      resolvers: myResolvers,
+    }
   });
 }
 export default register;
