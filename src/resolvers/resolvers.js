744156c78a6d359fe37ae3f323647e5446d75895
shoaibfarooq3 <shoaib.farooq@codistan.org>
2023-03-06 15:46:56 +0500

    ShoaibNewPlugin

--- src/resolvers/resolvers.js
+++ src/resolvers/resolvers.js
@@ -1,15 +1,73 @@
+import ObjectID from "mongodb";
 export default {
     Mutation: {
-        createOrder: async (_, { startTime, endTime, status }) => {
-            console.log("start Time" + startTime)
-            console.log("End Time" + endTime)
-            console.log("status " + status)
-            const order = { startTime, endTime, status };
-            let { RiderOrder } = context.collections;
-            const result = await RiderOrder.insertOne(order);
-            console.log("result " + result)
-            const newOrder = { id: result.insertedId, ...order };
-            return newOrder;
+        async createRiderOrder(parent, { orders }, context, info) {
+            console.log(orders)
+            // console.log(context.user.id)
+            const { RiderOrder, RiderOrderHistory } = context.collections;
+            const CurrentRiderID = context.user.id;
+            const insertedOrders = await RiderOrder.insertMany(orders);
+            console.log(insertedOrders.insertedIds)
+            console.log(insertedOrders)
+            const createdOrderIDs = {
+                OrderID: insertedOrders.insertedIds,
+                RiderID: CurrentRiderID,
+            }
+            await RiderOrderHistory.insertOne(createdOrderIDs);
+            console.log(RiderOrderHistory);
+            return insertedOrders.ops;
+        },
+        async updateRiderOrder(parent, { id, startTime, endTime, OrderStatus, RiderOrderID }, context, info) {
+            const { RiderOrder } = context.collections;
+            const filter = { _id: ObjectID.ObjectId(id) };
+            const update = {};
+            if (startTime) {
+                update.startTime = startTime;
+            }
+            if (endTime) {
+                update.endTime = endTime;
+            }
+            if (OrderStatus) {
+                update.OrderStatus = OrderStatus;
+            }
+            if (RiderOrderID) {
+                update.RiderOrderID = RiderOrderID;
+            }
+            const options = { returnOriginal: false };
+            const response = await RiderOrder.findOneAndUpdate(filter, { $set: update }, options);
+            console.log(response)
+            console.log(response.value)
+            return {
+                id: response.value._id,
+                startTime: response.value.startTime,
+                endTime: response.value.endTime,
+                OrderStatus: response.value.OrderStatus,
+                RiderOrderID: response.value.RiderOrderID,
+            };
+        },
+
+    },
+    Query: {
+        async getOrderById(parent, { id }, context, info) {
+            const { RiderOrder } = context.collections;
+            const ordersresp = await RiderOrder.find({ LoginRiderID: id }).toArray();
+            console.log(ordersresp)
+            if (ordersresp) {
+                return ordersresp
+            } else {
+                return null;
+            }
+        },
+        async getOrdersByStatus(parent, { OrderStatus }, context, info) {
+            console.log(OrderStatus)
+            const { RiderOrder } = context.collections;
+            const orders = await RiderOrder.find({ OrderStatus: OrderStatus }).toArray();
+            console.log(orders)
+            if (orders) {
+                return orders.map(order => ({ id: order._id, OrderStatus: order.OrderStatus ? order.OrderStatus : null, ...order }));
+            } else {
+                return null;
+            }
         },
     },
 }
