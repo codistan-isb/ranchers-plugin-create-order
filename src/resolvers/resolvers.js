569c3b83a7aa121bc20793b7ccc2512c4a5bc494
shoaibfarooq3 <shoaib.farooq@codistan.org>
2023-03-14 12:24:54 +0500

    NewUpdationInUser

--- src/resolvers/resolvers.js
+++ src/resolvers/resolvers.js
@@ -31,7 +31,12 @@ export default {
             console.log(RiderOrderHistory);
             return insertedOrders.ops;
         },
-        async updateRiderOrder(parent, { id, startTime, endTime, OrderStatus, RiderOrderID }, context, info) {
+        async updateRiderOrder(
+            parent,
+            { id, startTime, endTime, OrderStatus, RiderOrderID },
+            context,
+            info
+        ) {
             const { RiderOrder } = context.collections;
             const filter = { RiderOrderID: RiderOrderID };
             const update = {};
@@ -48,48 +53,82 @@ export default {
                 update.RiderOrderID = RiderOrderID;
             }
             const options = { returnOriginal: false };
-            const response = await RiderOrder.findOneAndUpdate(filter, { $set: update }, options);
-            console.log(response)
-            console.log(response.value)
+            const response = await RiderOrder.findOneAndUpdate(
+                filter,
+                { $set: update },
+                options
+            );
+            console.log(response);
+            console.log(response.value);
             return {
                 id: response.value._id,
                 startTime: response.value.startTime,
                 endTime: response.value.endTime,
                 OrderStatus: response.value.OrderStatus,
                 RiderOrderID: response.value.RiderOrderID,
+                LoginRiderID: response.value.LoginRiderID,
+                OrderID: response.value.OrderID,
             };
         },
+        async updateUserCurrentStatus(parent, args, context, info) {
+            const { users } = context.collections;
+            console.log(args.status)
+            console.log(context.user.id)
+            const currentStatus = args.status
+            const userID = context.user.id;
+            if (!userID) {
+                throw new Error('Please Login First');
+            }
+            const updatedUser = await users.findOneAndUpdate(
+                { _id: userID },
+                { $set: { currentStatus } },
+                { returnOriginal: false }
+            );
+            console.log(updatedUser.value)
+            if (!updatedUser) {
+                throw new Error(`User with ID ${userID} not found`);
+            }
 
+            return updatedUser.value;
+            //     const updatedUser = await users.findOneAndUpdate(
+            //         { _id: ObjectID(userID) },
+            //         { $set: { currentStatus: status } },
+            //         { returnOriginal: false }
+            //     );
+
+            //     return updatedUser;
+            // }
+        },
     },
     Query: {
         async getOrderById(parent, { id }, context, info) {
             const { RiderOrder } = context.collections;
             const ordersresp = await RiderOrder.find({ LoginRiderID: id }).toArray();
-            console.log(ordersresp)
+            console.log(ordersresp);
             if (ordersresp) {
-                return ordersresp
+                return ordersresp;
             } else {
                 return null;
             }
         },
         async getOrdersByStatus(parent, { OrderStatus }, context, info) {
-            console.log(OrderStatus)
-            console.log(context.user.id)
-            const LoginUserID = context.user.id
+            console.log(OrderStatus);
+            console.log(context.user.id);
+            const LoginUserID = context.user.id;
             const { RiderOrder } = context.collections;
-            const orders = await RiderOrder.find({ OrderStatus: OrderStatus }).toArray();
-            console.log(orders)
+            const orders = await RiderOrder.find({
+                OrderStatus: OrderStatus,
+            }).toArray();
+            console.log(orders);
             if (orders) {
-                const filteredOrders = orders.filter(order => order.LoginRiderID === LoginUserID);
-                const ordersWithId = filteredOrders.map(order => ({
+                const filteredOrders = orders.filter(
+                    (order) => order.LoginRiderID === LoginUserID
+                );
+                const ordersWithId = filteredOrders.map((order) => ({
                     id: order._id,
-                    ...order
+                    ...order,
                 }));
-                return ordersWithId
-                // return orders.map(order => ({
-                //     id: order._id,
-                //     ...order
-                // }));
+                return ordersWithId;
             } else {
                 return null;
             }
@@ -97,36 +136,38 @@ export default {
         async generateOrderReport(parent, args, context, info) {
             const { RiderOrder, Users } = context.collections;
             const { id } = context.user;
-            console.log(id)
+            console.log(id);
             const report = await RiderOrder.aggregate([
                 {
                     $match: { LoginRiderID: id },
                 },
                 {
                     $lookup: {
-                        from: 'users',
-                        localField: 'LoginRiderID',
-                        foreignField: '_id',
-                        as: 'Rider',
+                        from: "users",
+                        localField: "LoginRiderID",
+                        foreignField: "_id",
+                        as: "Rider",
                     },
                 },
                 {
-                    $unwind: '$Rider',
+                    $unwind: "$Rider",
                 },
                 {
                     $project: {
-                        LoginRiderID: '$LoginRiderID',
-                        riderName: '$Rider.username',
-                        branchCity: '$Rider.branchCity',
-                        branchName: '$Rider.branchname',
-                        orderStatus: '$OrderStatus',
-                        startTime: { $toDate: '$startTime' },
-                        endTime: { $toDate: '$endTime' },
+                        LoginRiderID: "$LoginRiderID",
+                        riderName: "$Rider.username",
+                        branchCity: "$Rider.branchCity",
+                        branchName: "$Rider.branchname",
+                        orderStatus: "$OrderStatus",
+                        startTime: { $toDate: "$startTime" },
+                        endTime: { $toDate: "$endTime" },
                     },
                 },
                 {
                     $addFields: {
-                        deliveryTime: { $divide: [{ $subtract: ['$endTime', '$startTime'] }, 60000] },
+                        deliveryTime: {
+                            $divide: [{ $subtract: ["$endTime", "$startTime"] }, 60000],
+                        },
 
                         // deliveryTime: { $subtract: ['$endTime', '$startTime'] },
                     },
@@ -135,8 +176,6 @@ export default {
 
             console.log(report);
             return report;
-        }
-
-
+        },
     },
-}
+};
