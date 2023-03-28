9a9251367d3db5806005b3011feacb57784adcfe
shoaibfarooq3 <shoaib.farooq@codistan.org>
2023-03-28 21:26:56 +0500

    NewAPIUpdation

--- src/resolvers/resolvers.js
+++ src/resolvers/resolvers.js
@@ -11,6 +11,8 @@ export default {
             const { RiderOrder, RiderOrderHistory, Accounts } = context.collections;
             const CurrentRiderID = context.user.id;
             const currentDate = new Date().toISOString().substr(0, 10);
+            const currentOrderDate = new Date().toISOString();
+
             console.log(currentDate);
             const riderStatus = await Accounts.findOne({ _id: CurrentRiderID });
             if (riderStatus.currentStatus === "offline") {
@@ -19,6 +21,7 @@ export default {
             const ordersWithRiderId = orders.map((order) => ({
                 ...order,
                 LoginRiderID: CurrentRiderID,
+                createdAt: currentOrderDate,
             }));
 
             const existingOrders = await RiderOrder.find({
@@ -390,43 +393,44 @@ export default {
             console.log(filteredData);
             return filteredData;
         },
-        // async getBranchOwnerReport(parent, args, context, info) {
-        //     // console.log(context.collections)
-        //     console.log(args);
-        //     const { branchID, orderStatus } = args;
-        //     console.log(context.user);
-        //     // console.log(context.user.UserRole);
-        //     console.log(!context.user.branches);
-        //     if (context.user === undefined || context.user === null) {
-        //         throw new Error("Unauthorized access. Please login first");
-        //     }
-        //     if (
-        //         context.user.UserRole !== "admin" &&
-        //         (!context.user.branches ||
-        //             (context.user.branches && !context.user.branches.includes(branchID)))
-        //     ) {
-        //         throw new Error(
-        //             "Only admins or authorized branch users can access orders report"
-        //         );
-        //     }
-        //     const { BranchData, Orders } = context.collections;
-        //     const query = {};
-        //     if (branchID) {
-        //         query.branchID = branchID;
-        //     } else if (user.branches) {
-        //         query.branchID = { $in: user.branches };
-        //     }
-        //     if (orderStatus) {
-        //         query.status = orderStatus;
-        //     }
-        //     console.log(query);
-        //     const ordersResp = await Orders.find(query).sort({ createdAt: -1 }).toArray();
-        //     console.log(ordersResp)
-        //     const ordersWithId = ordersResp.map((order) => ({
-        //         id: order._id,
-        //         ...order,
-        //     }));
-        //     return ordersWithId
-        // },
+        async getBranchOwnerReport(parent, args, context, info) {
+            // console.log(context.collections)
+            console.log(args);
+            const { branchID, orderStatus } = args;
+            console.log(context.user);
+            // console.log(context.user.UserRole);
+            // console.log(!context.user.branches);
+            if (context.user === undefined || context.user === null) {
+                throw new Error("Unauthorized access. Please login first");
+            }
+            if (
+                context.user.UserRole !== "admin" &&
+                (!context.user.branches ||
+                    (context.user.branches && !context.user.branches.includes(branchID)))
+            ) {
+                throw new Error(
+                    "Only admins or authorized branch users can access orders report"
+                );
+            }
+            const { BranchData, Orders } = context.collections;
+            const query = {};
+            if (branchID) {
+                query.branchID = branchID;
+            } 
+            // else if (user.branches) {
+            //     query.branchID = { $in: user.branches };
+            // }
+            if (orderStatus) {
+                query.status = orderStatus;
+            }
+            console.log(query);
+            const ordersResp = await Orders.find(query).sort({ createdAt: -1 }).toArray();
+            console.log(ordersResp)
+            const ordersWithId = ordersResp.map((order) => ({
+                id: order._id,
+                ...order,
+            }));
+            return ordersWithId
+        },
     },
 };
